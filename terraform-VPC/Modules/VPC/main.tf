5853d673f1d3d11eb6b8bb9aaf1d49da10a4308e
prabakaran181993 <prabawatson@gmail.com>
2026-05-04 17:31:47 +0530

    test

---
+++ terraform-VPC/Modules/VPC/main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "test_vpc" {
+    cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
+    tags = {
+        "name" = "test_vpc"
+
+    }
+}
+
+resource "aws_subnet" "subnets" {
+    count = length(var.subnet_cidr) 
+  vpc_id     = aws_vpc.test_vpc.id
+  cidr_block = var.subnet_cidr [count.index]
+  
+  tags = {
+    Name = "Main"
+  }
+}
---
+++ terraform-VPC/Modules/VPC/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "cidr range for vpc
+  "
+} 
+
+variable "subnet_cidr" {
+    description = "subnets cidr"
+    type = list(string)
+
+}
+
